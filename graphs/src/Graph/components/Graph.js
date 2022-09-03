a7263e66b9dbec377157e17870db94fa83372a4b
Oana Obloja <oblojaoana98@gmail.com>
2022-09-03 21:02:14 +0300

    data manipulation - number of posts created in each month of 2019

--- graphs/src/Graph/components/Graph.js
+++ graphs/src/Graph/components/Graph.js
@@ -1,20 +1,30 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { LOAD_POSTS } from "../gql/Queries";
-
+import { monthsNames } from "./monthsNames";
 
 function Graph() {
-  const { error, loading, data } = useQuery(LOAD_POSTS);
-   
-  
-  return (
-    <div>
-      {/* {" "}
-      {users.map((val) => {
-        return <h1> {val.firstName}</h1>;
-      })} */}
-    </div>
-  );
+  const { loading, data } = useQuery(LOAD_POSTS);
+
+  if (loading) return <div></div>;
+
+  const filterPosts = data.allPosts.filter((item) => {
+    return new Date(parseInt(item.createdAt)).getFullYear() === 2019;
+  });
+
+  const rawMonthData = filterPosts.reduce((acc, el) => {
+    acc[new Date(parseInt(el.createdAt)).getMonth()]++;
+    return acc;
+  }, new Array(12).fill(0));
+
+  const statData = rawMonthData.map((el, idx) => {
+    return {
+      month: monthsNames[idx],
+      nr: el,
+    };
+  });
+
+  return <div></div>;
 }
 
 export default Graph;
--- graphs/src/Graph/gql/Queries.js
+++ graphs/src/Graph/gql/Queries.js
@@ -3,18 +3,9 @@ import { gql } from "@apollo/client";
 
 export const LOAD_POSTS= gql`
   query {
-    allPosts(count: 5) {
+    allPosts(count: 50) {
     id
-    title
-    body
-    published
     createdAt
-    author {
-      id
-      firstName
-      lastName
-      avatar
-    }
   }
 }
 `;
